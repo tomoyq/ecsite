64d2cc8437fa770fdc0ab5f20f48446b3b83b21f
tomoya <arata5.18830@gmail.com>
2024-07-04 18:03:16 +0900

    モデルの作成が完了

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Item(models.Model):
+    name = models.CharField(max_length = 100)
+    price = models.PositiveIntegerField()
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='item_images/', blank=True, null=True)
+
+class CartItem(models.Model):
+    item = models.ForeignKey(to=Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+class Cart(models.Model):
+    cart_item = models.ManyToManyField(to=CartItem, blank=True)
+
+class Order(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    order_item = models.ManyToManyField(to=CartItem)
+    order_price = models.PositiveIntegerField()
+    order_date = models.DateTimeField(auto_now=True)
