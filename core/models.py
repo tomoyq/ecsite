41480200c2826d266c3313ccce20949e84c8f295
tomoya <arata5.18830@gmail.com>
2024-07-25 18:21:05 +0900

    ビューの作成が完了

--- core/models.py
+++ core/models.py
@@ -16,8 +16,25 @@ class CartItem(models.Model):
     item = models.ForeignKey(to=Item, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
+    @property
+    def total_price(self):
+        return self.item.price * self.quantity
+
 class Cart(models.Model):
     cart_item = models.ManyToManyField(to=CartItem, blank=True)
+
+    def add_cart_item(self, new_cart_item):
+        if new_cart_item.item in [cart_item.item for cart_item in self.cart_item.all()]:
+            original_cart_item = self.cart_item.get(item_id=new_cart_item.item.id)
+            original_cart_item.quantity += new_cart_item.quantity
+            original_cart_item.save()
+        else:
+            new_cart_item.save()
+            self.cart_item.add(new_cart_item)
+
+    @property
+    def total_price(self):
+        return sum([cart_item.total_price for cart_item in self.cart_item.all()])
 
 class Order(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
--- core/views.py
+++ core/views.py
@@ -1,19 +1,65 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 
-from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import DetailView, ListView
+from django.views.generic.base import TemplateView, View
 
-class HomeView(TemplateView):
+from .models import Item, CartItem, Order
+
+User = get_user_model()
+
+class OnlyYouMixin(UserPassesTestMixin):
+    def test_func(self):
+        user = self.request.user
+        return (user.id == self.kwargs['pk']) or (user.is_superuser)
+
+class HomeView(ListView):
     template_name = 'core/home.html'
+    model = Item
 
-class DetailView(TemplateView):
+class DetailView(DetailView):
     template_name = 'core/detail.html'
+    model = Item
 
-class CartView(LoginRequiredMixin, TemplateView):
+    def get_success_url(self):
+        user_pk = self.request.user.id
+        return reverse_lazy('cart', kwargs={'pk': user_pk})
+
+    def post(self, *args, **kwargs):
+        item_pk = self.request.POST.get('item_pk')
+        quantity = int(self.request.POST.get('quantity'))
+        item = Item.objects.get(id=item_pk)
+        cart_item = CartItem(item=item, quantity=quantity)
+        user = self.request.user
+        user.cart.add_cart_item(cart_item)
+        return redirect(self.get_success_url())
+
+class CartView(LoginRequiredMixin, OnlyYouMixin, DetailView):
     template_name = 'core/cart.html'
+    model = User 
+    context_object_name = 'cart'
 
-class OrderView(RedirectView):
-    url = reverse_lazy('success')
+    def get_object(self, queryset=None):
+        user = super().get_object(queryset)
+        return user.cart
+
+class OrderView(View):
+    def post(self, *args, **kwargs):
+        order_user = self.request.user
+        order_cart = order_user.cart
+        order_obj = Order.objects.create(
+            user = order_user,
+            order_price = order_cart.total_price,
+        )
+        for cart_item in order_cart.cart_item.all():
+            order_obj.order_item.add(cart_item)
+        order_cart.cart_item.clear()
+
+        return redirect(reverse_lazy('success'))
 
 class SuccessView(TemplateView):
     template_name = 'core/success.html'
